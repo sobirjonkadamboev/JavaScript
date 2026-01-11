c454d2a00b8ddff0def42ce70bb76cb8f16c702f
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-11 16:19:07 +0000

    .

--- js/operators.js
+++ js/operators.js
@@ -1,3 +1,34 @@
-confirm('Are you 19+ years old?');
-
+//confirm('Are you above 19?');
 // alert('Hi, world');
+
+//const question = prompt('How old are you? ');
+//console.log(question);
+
+//CamelCase	
+/* const firstName = 'Sobirjon';
+const lastName = 'Kadamboev';
+const age = 19;
+
+console.log(`My full name is ${firstName, lastName} and I'm ${age} years old`);
+const username = "youtube.com"
+console.log(`subscribe to my channel https://${username}`); */
+
+const x = 10;
+const y = 2;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+
+console.log(x < y);
+console.log(x > y);
+console.log(x == y);
+console.log(x != y);
+
+
+
+
+
