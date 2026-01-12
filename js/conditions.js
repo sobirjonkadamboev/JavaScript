1c1dac2b7aa598f355f3673e30e6b2f24870ddec
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-12 14:42:12 +0000

    .

--- js/conditions.js
+++ js/conditions.js
@@ -1,15 +1,17 @@
-const light = 'green';
+/* const light = 'green';
 
 console.log(light === 'green');
 
 
-if(light == 'red'){
+if(light === 'red'){
 	console.log('Stop!');
-} else if(light == 'yellow'){
+} else if(light === 'yellow'){
 	console.log('Prepare!');
-} else if(light == 'green'){
+} else if(light === 'green'){
 	console.log('Goo!');
 }
+	== - bu ikkita qiymatga qarab tekshiradi
+	=== - bu qat'iy tekshiruv turi, ham ma'lumotga, ham qiymatni tekshiradi
 
 const age = +prompt('How old are you?', '');
 if(age <= 18){
@@ -17,4 +19,50 @@ if(age <= 18){
 }
 else{
 	console.log('Access');
+} 
+
+if(light === 'red'){
+	console.log('Stop!');
+} else if(light === 'yellow'){
+	console.log('Prepare!');
+} else if(light === 'green'){
+	console.log('Goo!');
+} 
+
+const light = 'yellow';
+
+switch(light){
+	case 'green':
+		console.log('Go.');
+		break
+	case 'red':
+		console.log('Stop!');
+		break
+	case 'yellow':
+		console.log('Prepare');
+		break
+	default:
+		console.log('Just go now.');
+} 
+
+let number = 0;
+
+while(number <= 10){
+	console.log(number);
+	number++;
+}
+
+do{
+	console.log(number);
+	number++;
+} while(number <= 10);
+
+*/
+let number = 0;
+for(number; number <= 10; number++){
+	if(number === 7){
+		break;
+	}
+	console.log(number);
+	
 }
