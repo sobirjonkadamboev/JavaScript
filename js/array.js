a48afc61e89bd4c1f40a2b90326787e5d041689f
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-16 07:20:34 +0000

    .

--- js/array.js
+++ js/array.js
@@ -1,4 +1,10 @@
 const colors = ['red', 'yellow', 'black', 'green', 'brown'];
 console.log('Array length: ', colors.length); // in array length, if there is 2, it adds one more.
-
+console.log('Choosing the specific aray: ', colors[4]);
 console.log(colors);
+
+
+//Pop and Push
+// 1.Pop - removes the last element of an array
+
+console.log('Removing the last element: ', colors.pop());
