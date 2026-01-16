d6b5beb0259e29b77fec06f6ab02ee436143bce3
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-16 07:28:46 +0000

    .

--- js/array.js
+++ js/array.js
@@ -1,10 +1,23 @@
 const colors = ['red', 'yellow', 'black', 'green', 'brown'];
 console.log('Array length: ', colors.length); // in array length, if there is 2, it adds one more.
-console.log('Choosing the specific aray: ', colors[4]);
-console.log(colors);
+console.log('Choosing the specific array: ', colors[4]);
+
 
 
 //Pop and Push
 // 1.Pop - removes the last element of an array
+// 2.Push - adds one element to an array
 
 console.log('Removing the last element: ', colors.pop());
+console.log('Adding new element:', colors.push('orange'));
+
+
+//Shift and Unshift
+// 1.Shift - removes the first element of an array
+// 2.Unshift - adds one element to start of an array
+console.log('Removing the first element: ', colors.shift());
+console.log('Adding one element from first: ', colors.unshift('gray'));
+
+
+console.log(colors);
+
