67bb06603d70cf55bb49be54c122f2495a7076a1
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-16 07:14:58 +0000

    ..

---
+++ js/array.js
@@ -0,0 +1,4 @@
+const colors = ['red', 'yellow', 'black', 'green', 'brown'];
+console.log('Array length: ', colors.length); // in array length, if there is 2, it adds one more.
+
+console.log(colors);
