e1d4103474fe308810728ab4dd89d3cf7047c530
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-17 17:38:00 +0000

    .

--- js/oop.js
+++ js/oop.js
@@ -1,1 +1,20 @@
-console.dir([1, 2, 3]);
+// console.dir([1, 2, 3]);
+
+const suvCars = {
+	weight: 1000,
+	length: 3,
+	speed: function(){
+		console.log('100 km/h');
+		
+	},
+};
+
+const gentra = {
+	weight: 800,
+};
+
+// gentra.__proto__ = suvCars; old version
+
+Object.setPrototypeOf(gentra, suvCars); //new version
+
+console.log(gentra.speed());
