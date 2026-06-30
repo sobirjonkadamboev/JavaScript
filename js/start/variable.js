d1fdea27d41b192b1dcb2e848c651a6b9b79e92f
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-06-30 05:31:05 +0000

    autosave

--- js/start/variable.js
+++ js/start/variable.js
@@ -2,9 +2,9 @@
 //let, const - types of variables
 
 let age = 19 //you can change 'let' variable
-console.log(age)
-
+let Age = 21
 age = 20
 console.log(age)
+console.log(Age)
 
 const name = 'Sobirjon' //you can't change 'const' variable without specific task
