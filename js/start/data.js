3e76083dde4ead76bfa8f0ae4801809f5758c7b6
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-02 04:23:51 +0000

    autosave

--- js/start/data.js
+++ js/start/data.js
@@ -1,5 +1,21 @@
-//Datatypes in JS: 7
+//Datatypes in JS: 8
 
 //1.Number
+const age = 20
+let number = 21
 
-let number = 5
+console.log(typeof age, typeof number)
+
+//2.String
+
+//3.Boolean
+
+//4.Null
+
+//5.Undefined
+
+//6.BigInt
+
+//7.Symbol
+
+//8.Object
