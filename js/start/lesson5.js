8eed0527577298305b0fe3106555647b48510679
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-07 03:52:35 +0000

    autosave

--- js/start/lesson5.js
+++ js/start/lesson5.js
@@ -6,3 +6,8 @@ const name = prompt('Your Name?')
 const age = +prompt('Your Age?', '') //put + for number datatype //NaN - not a number
 const age = +prompt('Your Age?', '0')
 console.log(typeof age)
+
+//Interpolation in JS
+
+const fName = 'Sobirjon'
+const lName = 'Kadamboev'
