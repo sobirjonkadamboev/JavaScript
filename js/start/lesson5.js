3a22d25545e52f951413c64a94c1a3005a92ccb7
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-07 04:02:45 +0000

    autosave

--- js/start/lesson5.js
+++ js/start/lesson5.js
@@ -1,11 +1,11 @@
 //alert('Alert Message')
 
-// const isAccess = confirm('Are you human?')
+/* const isAccess = confirm('Are you human?')
 
 const name = prompt('Your Name?')
 const age = +prompt('Your Age?', '') //put + for number datatype //NaN - not a number
 console.log(typeof age)
-
+*/
 //Interpolation in JS
 
 const fName = prompt('Your Name?')
