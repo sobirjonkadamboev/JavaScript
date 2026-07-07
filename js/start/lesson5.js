456747444162e89b7648e40bc40b916cc8f6ee17
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-07 03:49:27 +0000

    autosave

--- js/start/lesson5.js
+++ js/start/lesson5.js
@@ -3,5 +3,5 @@
 // const isAccess = confirm('Are you human?')
 
 const name = prompt('Your Name?')
-const age = +prompt('Your Age?') //put + for number datatype
+const age = +prompt('Your Age?') //put + for number datatype //NaN - not a number
 console.log(typeof age)
