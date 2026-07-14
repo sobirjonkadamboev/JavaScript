1a1c6079d4025e1ffcd88a921f9dfd6c4ffa3f0f
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-14 03:45:37 +0000

    autosave

--- js/start/functions.js
+++ js/start/functions.js
@@ -3,22 +3,17 @@
 function showHello() {
 	console.log('Hello, World')
 }
-
 let age = 20 //global variable
-
 function userGreet(name) {
 	age = 19 //local variable
 	console.log(`Hello, ${name}! He is ${age} years old`)
 }
-
 showHello()
 userGreet('Any Name Here')
 console.log(age)
-
 function calc(a, b) {
 	console.log(a + b)
 	// return a + b //after this, no code will run
 }
-
 const firstCalc = calc(2, 5)
 console.log(firstCalc)
