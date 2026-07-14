669d51bad5b22dc074e7af06be7986c6005459df
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-14 03:16:31 +0000

    autosave

--- js/start/assignment.js
+++ js/start/assignment.js
@@ -10,10 +10,11 @@ const seriesDB = {
 for (let i = 0; i < 2; i++) {
 	const a = prompt('Oxirgi korgan serialingiz', '')
 	const b = +prompt('Nechi baxo berasiz?', '')
+	seriesDB.series[a] = b
+	seriesDB.series[c] = d
 }
 /*
 
-seriesDB.series[a] = b
-seriesDB.series[c] = d */
+ */
 
 console.log(seriesDB)
