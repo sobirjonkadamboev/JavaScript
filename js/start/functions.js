c5b359556208222c69bcb8935fd1f6ec6fb16ec2
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-14 03:34:58 +0000

    autosave

--- js/start/functions.js
+++ js/start/functions.js
@@ -4,6 +4,7 @@ function showHello() {
 	console.log('Hello, World')
 }
 
+const age = 20 //global variable
 function userGreet(name) {
 	const age = 19 //local variable
 	console.log(`Hello, ${name}! He is ${age} years old`)
