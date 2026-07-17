8c384b7c5bcb77dc3a4c062ca304fc5792ad9761
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-17 02:33:36 +0000

    autosave

--- js/start/methods.js
+++ js/start/methods.js
@@ -7,5 +7,6 @@ console.log(test.at(5)) //same
 console.log(test[5]) //same
 console.log(test.toUpperCase()) //upperCase
 console.log(test.toLowerCase()) //lowerCase
-console.log(test.slice(0, 7)) //to trim, slice
+console.log(test.slice(0, 7)) //to slice
 console.log(test.substring(0, 7)) //can't use -7 here
+console.log(password.trim())
