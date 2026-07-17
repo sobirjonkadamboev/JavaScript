751d01193df91243b0c7e6409f2a187e74135372
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-17 02:29:28 +0000

    autosave

--- js/start/methods.js
+++ js/start/methods.js
@@ -6,4 +6,4 @@ console.log(test.at(5)) //same
 console.log(test[5]) //same
 console.log(test.toUpperCase()) //upperCase
 console.log(test.toLowerCase()) //lowerCase
-console.log(test.slice(0, 7))
+console.log(test.slice(0, 7)) //to trim, slice
