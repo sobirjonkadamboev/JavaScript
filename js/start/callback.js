b3080fdc59c55b0bab628fb569f718e7369defd7
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-22 03:44:14 +0000

    autosave

--- js/start/callback.js
+++ js/start/callback.js
@@ -40,7 +40,7 @@ const person = {
 	},
 }
 
-console.log(Object.keys(person))
+console.log(Object.keys(person).length)
 //for in -> only for objects
 //for of -> only for arrays
 for (let key in person) {
