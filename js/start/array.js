4b01e50d7d04dccf0c52b2d1425e03847e94df1f
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-23 02:20:16 +0000

    autosave

--- js/start/array.js
+++ js/start/array.js
@@ -1,5 +1,11 @@
 const colors = ['green', 'yellow', 'red', 'blue', 'navy blue']
+
 console.log('Array Length:', colors.length)
 console.log('Element Index:', colors[3])
 
+//Pop and Push methods
+//Pop - removes the last element
+//Push -
+
+console.log('Removing Element:', colors.pop())
 console.log(colors)
