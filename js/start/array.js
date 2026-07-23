6e540c8de68ee13f363981d28c0355045ed5791f
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-23 02:18:39 +0000

    autosave

--- js/start/array.js
+++ js/start/array.js
@@ -1,5 +1,5 @@
 const colors = ['green', 'yellow', 'red', 'blue', 'navy blue']
 console.log('Array Length:', colors.length)
-console.log('Element Index', colors[2])
+console.log('Element Index:', colors[2])
 
 console.log(colors)
