288efa172e84da064dee6483e495387ac8bc59eb
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-24 03:09:37 +0000

    autosave

--- js/start/spread.js
+++ js/start/spread.js
@@ -5,13 +5,8 @@ b += 10
 console.log(a)
 console.log(b)
 
+//Object clone: deep and shallow
 const obJect = {
 	name: 'Sobirjon',
 	age: 19,
 }
-
-const sobJect = obJect
-sobJect.name = 'Afruz'
-sobJect.age = 20
-console.log(obJect)
-console.log(sobJect)
