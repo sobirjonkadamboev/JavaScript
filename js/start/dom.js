12b642e4fcab72d4a42de7996c728d3d9644d84e
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-27 06:58:39 +0000

    autosave

--- js/start/dom.js
+++ js/start/dom.js
@@ -20,4 +20,4 @@ console.log(hearts) */
 const box = document.querySelector('#box'),
 	buttons = document.querySelectorAll('.button')
 
-box.style.backgroundColor = 'navy blue'
+box.style.backgroundColor = 'blue'
