e13e7983c66e393e72bd1d0461fa3ee7b7a83613
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-28 05:00:59 +0000

    autosave

--- js/start/dom.js
+++ js/start/dom.js
@@ -43,4 +43,4 @@ buttons.forEach(item => {
 const div = document.createElement('div')
 div.classList.add('heart')
 document.querySelector('.wrapper').append(div)
-document.body.append(div) //adds an element to the body
+//document.body.append(div) //adds an element to the body
