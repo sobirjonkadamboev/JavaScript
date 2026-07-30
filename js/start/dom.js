968ffce4d51a636bbcd6e9c1c9a86d28098d79fa
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-30 04:40:00 +0000

    autosave

--- js/start/dom.js
+++ js/start/dom.js
@@ -63,4 +63,4 @@ document.querySelector('.app').append(div)
 
 //div.innerHTML = '<h1>Hi, AI</h1>' //works with any html elements
 
-div.textContent = 'hi,text'
+div.textContent = 'hi,text' //works with only string
