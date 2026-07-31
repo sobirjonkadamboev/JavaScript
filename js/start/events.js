ba8074000a21797eb7bbf91c9972eb9ddde0d5f3
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-31 03:45:16 +0000

    autosave

--- js/start/events.js
+++ js/start/events.js
@@ -8,12 +8,14 @@ btn1.onmousedown = function () {
 	alert('mouse')
 } */
 
-btn1.addEventListener('click', event => {
+const callback = function () {
 	console.log(event.target) //pointer events
 	event.target.textContent = 'Text'
 	event.target.style.backgroundColor = 'red'
 	console.log('first click')
-})
+}
+
+btn1.addEventListener('click', callback())
 
 /* btn1.addEventListener('click', () => {
 	console.log('second click')
