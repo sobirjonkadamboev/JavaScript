bcbab79b3fed560d44548a0873cd0d302bf9c06e
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-07-31 03:43:00 +0000

    autosave

--- js/start/events.js
+++ js/start/events.js
@@ -10,6 +10,7 @@ btn1.onmousedown = function () {
 
 btn1.addEventListener('click', event => {
 	console.log(event.target) //pointer events
+	event.target.textContent = 'Text'
 	console.log('first click')
 })
 
