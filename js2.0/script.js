5ab0fba5b779fc85a491dc586fac010833f5602e
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-03 07:31:44 +0000

    autosave

--- js2.0/script.js
+++ js2.0/script.js
@@ -3,4 +3,4 @@ const buttons = document.querySelectorAll('button')
 console.log(buttons[0].classList.value) //hamma class valuelarni chiqaradi
 console.log(buttons[0].classList.item(1)) //class listdagi itemlar bilan ishlash
 console.log(buttons[0].classList.add('yellow')) //yoki remove() o'chirish uchun
-console.log(buttons[0].classList.toggle('blue')) //bor narsani yo'q, yo'q narsani bor qiladi
+console.log(buttons[0].classList.toggle('blue')) //bor narsani yo'q, yo'q narsani bor qiladi (value)
