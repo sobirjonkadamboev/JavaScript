2781304730368bf2fa076bd205a68af572327fe7
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-03 07:53:22 +0000

    autosave

--- js2.0/script.js
+++ js2.0/script.js
@@ -15,7 +15,7 @@ wrapper.addEventListener('click', event => {
 })
 
 const newButton = document.createElement('button')
-
+wrapper.append(newButton)
 /* console.log(buttons[0].classList.value) //hamma class valuelarni chiqaradi
 console.log(buttons[0].classList.item(1)) //class listdagi itemlar bilan ishlash
 console.log(buttons[0].classList.add('yellow')) //yoki remove() o'chirish uchun
