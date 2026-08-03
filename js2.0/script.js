79afb6f8c24573381e368dd55a26c3b394b8f2e3
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-03 07:36:35 +0000

    autosave

--- js2.0/script.js
+++ js2.0/script.js
@@ -5,8 +5,18 @@ console.log(buttons[0].classList.item(1)) //class listdagi itemlar bilan ishlash
 console.log(buttons[0].classList.add('yellow')) //yoki remove() o'chirish uchun
 console.log(buttons[0].classList.toggle('blue')) //bor narsani yo'q, yo'q narsani bor qiladi (value)
 
-*/
+
 
 if (buttons[0].classList.contains('red')) {
 	console.log('true')
-}
+} //shu element class ichida borligini aniqlaydi
+
+*/
+
+buttons[0].addEventListener('click', () => {
+	if (buttons[0].classList.contains('red')) {
+		console.log('contain red class')
+	} else {
+		console.log('not red class')
+	}
+})
