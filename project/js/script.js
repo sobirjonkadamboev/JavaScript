1cce4b60ec8450a77ea385534b6c39612888070a
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-04 03:58:18 +0000

    autosave

--- project/js/script.js
+++ project/js/script.js
@@ -2,4 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabContent = document.querySelectorAll('.tab_content'),
 		tabParents = document.querySelector('.tabheader__items')
+
+	function hideTabContents() {
+		tabContent.forEach(tabContent => {
+			tabContent.style.display = 'none'
+		})
+	}
+
+	hideTabContents()
 })
