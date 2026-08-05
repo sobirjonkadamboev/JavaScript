fbf2062460bdd900042bb3cece0ff738c448495b
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-05 03:59:54 +0000

    autosave

--- js2.0/animation_interval.js
+++ js2.0/animation_interval.js
@@ -2,10 +2,14 @@
 	console.log('Hi, this is timeout')
 }, 3000).  1-usuli */
 
+const btn = document.querySelector('#btn')
+
+btn.addEventListener('click', () => {
+	const newID = setTimeout(log, 2000) //2-usul
+})
+
 function log() {
 	console.log('Timeout 2s')
 }
-
-const newID = setTimeout(log, 2000) //2-usul
 
 clearInterval(newID)
