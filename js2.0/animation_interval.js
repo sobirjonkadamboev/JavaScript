d29a0d4dff007c7033db24338049c5b52fa2fa29
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-05 04:04:38 +0000

    autosave

--- js2.0/animation_interval.js
+++ js2.0/animation_interval.js
@@ -8,7 +8,7 @@ let theTimerID,
 	index = 0
 
 btn.addEventListener('click', () => {
-	theTimerID = setInterval(log, 1000) //2-usul
+	theTimerID = setTimeout(log, 1000) //2-usul
 })
 
 function log() {
