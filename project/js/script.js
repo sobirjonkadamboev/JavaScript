9d79f6e7b1e2c5aa1e15afb252695eceea745573
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-06 05:06:09 +0000

    autosave

--- project/js/script.js
+++ project/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+	//Tabs
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabContent = document.querySelectorAll('.tab_content'),
 		tabParents = document.querySelector('.tabheader__items')
