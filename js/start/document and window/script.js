d3f320b23a55b2471049b2962809495f53c1cb6d
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-09 03:16:01 +0000

    autosave

--- js/start/document and window/script.js
+++ js/start/document and window/script.js
@@ -3,8 +3,8 @@ const box = document.querySelector('.box')
 //const width = box.clientWidth
 //const height = box.clientHeight -> bu foydalanuvchi ko'radigan eni va bo'y qismi
 
-const width = box.offsetWidth
-const height = box.offsetHeight
+/* const width = box.offsetWidth
+const height = box.offsetHeight  -> bu faqat contentni width bn height*/
 
 console.log(width)
 console.log(height)
