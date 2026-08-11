683b5497de04508733cd14d5e25b0d637dc5e2ba
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-11 02:37:57 +0000

    autosave

--- js/start/contextThis.js
+++ js/start/contextThis.js
@@ -9,6 +9,7 @@ logThis()
 //#1. function dagi context this window objectga boradi, "use strict" modeda undefined bo'ladi
 //#2. context this objectlarda objectlar methodi bilan teng bo'ladi
 //#3. context this function constructor ichida yangi obyektni misollari
+//#4. call va apply methodlar bu functionga contextni bog'lash uchun ishlatiladi
 const person = {
 	fName: 'Sobirjon',
 	lName: 'Kadamboev',
