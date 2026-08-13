d379390316ac7111692a446cf300e80d889f2581
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-13 04:50:57 +0000

    autosave

--- js advanced/json.js
+++ js advanced/json.js
@@ -24,5 +24,5 @@ sumVal.addEventListener('input', () => {
 	const request = new XMLHttpRequest()
 
 	request.open('GET', './current.json')
-	request.setRequestHeader('Content-type', 'application/json')
+	request.setRequestHeader('Content-Type', 'application/json')
 })
