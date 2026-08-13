435988f9b50d8c1a3137c431486a7e773e34067f
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-13 04:27:26 +0000

    autosave

--- js advanced/json.js
+++ js advanced/json.js
@@ -7,6 +7,8 @@ const person = {
 	},
 }
 
+const deepClone = JSON.stringify(person)
+
 const objJson = JSON.stringify(person)
 const jsonObj = JSON.parse(objJson)
 //JSON, xml - serverga ma'lumot yuborishda ishlatiladi va ma'lumot JSON formatda bo'lishi shart
