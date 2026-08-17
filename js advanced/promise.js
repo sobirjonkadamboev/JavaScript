1c3c57d07366db610b0116c405e9c6bf6f092c63
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-17 05:21:03 +0000

    autosave

--- js advanced/promise.js
+++ js advanced/promise.js
@@ -7,7 +7,7 @@ const friend = {
 const request = new Promise((resolve, reject) => {
 	if (friendStatus === 'success') {
 		friend.time = '10:00'
-		resolve()
+		resolve(friend)
 	} else {
 		reject()
 	}
