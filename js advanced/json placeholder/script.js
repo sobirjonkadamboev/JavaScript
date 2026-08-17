6d310cd66b20419edb1f346d207fa82620736a38
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-17 14:09:37 +0000

    autosave

--- js advanced/json placeholder/script.js
+++ js advanced/json placeholder/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const postWrapper = document.querySelector('.posts')
 
 	//GET methodi:
-	fetch('https://jsonplaceholder.typicode.com1/posts', {
+	fetch('https://jsonplaceholder.typicode.com/posts', {
 		//fetch - bu serverdan ma'lumot olganda ishlatiladi, ya'ni serverga so'rov yuboriladi
 		method: 'GET',
 		headers: {
