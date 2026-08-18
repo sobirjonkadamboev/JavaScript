7b610e0612ec5c597f4d5692e4b5d6af6ce66eba
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-18 02:01:01 +0000

    autosave

--- js advanced/json placeholder/script.js
+++ js advanced/json placeholder/script.js
@@ -50,15 +50,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 			.then(res => res.json())
 			.then(data => {
-			data.forEach((item, index) => {
-				const postEl = document.createElement('div')
-				postEl.classList.add('post')
-				postEl.innerHTML += `<h4> <b>#1.
+				data.forEach((item, index) => {
+					const postEl = document.createElement('div')
+					postEl.classList.add('post')
+					postEl.innerHTML += `<h4> <b>#1.
 				${index + 1}. </b> ${item.title}</h4>
 			<p>${item.body}</p>`
-				postWrapper.append(postEl)
+					postWrapper.append(postEl)
+				})
 			})
-		}))
 			.catch(err => console.log(err))
 	})
 })
