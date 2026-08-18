5f90aa5c22b835827ec7c48a858158ff547852fe
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-18 02:06:23 +0000

    autosave

--- js advanced/json placeholder/script.js
+++ js advanced/json placeholder/script.js
@@ -50,6 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 			.then(res => res.json())
 			.then(data => {
+				form.reset()
 				const postEl = document.createElement('div')
 				postEl.classList.add('post')
 				postEl.innerHTML += `<h4> <b>#1.
