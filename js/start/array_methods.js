8dc83efcb3d774bb4027193ab814b6de379689dd
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-20 06:03:50 +0000

    autosave

--- js/start/array_methods.js
+++ js/start/array_methods.js
@@ -1,8 +1,25 @@
 //map, filter, reduce - ma'lumot qaytaradi
 //forEach - ma'lumot qaytarmaydi, iteratsiya qiladi
 
-//Map
+/* Map
 
 const arr = ['BMW', 'LAMBORGHINI', 'HONDA', 'LEXUS']
-const result = arr.map(item => item.toLowerCase())
-console.log(result)
+const result = arr.map(item => item.toLowerCase()) //arrayni olib uni yangi arrayga o'zgartira oladi
+console.log(result) */
+
+
+//filter
+const arr = [{
+	name: 'BMW',
+	model: 2023,	
+},
+{
+	name: 'TOYOTA',
+	model: 2023,
+}
+{
+	name: 'SUZUKI',
+	model: 2023,
+}
+
+]
