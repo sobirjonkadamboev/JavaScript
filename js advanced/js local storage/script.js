f54e7ea49da2641ebb84fcb6f966f9383719e190
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-26 01:33:03 +0000

    autosave

--- js advanced/js local storage/script.js
+++ js advanced/js local storage/script.js
@@ -4,3 +4,7 @@ localStorage.setItem('sobirjon-theme', 'navy blue')
 
 const theme = localStorage.getItem('sobirjon-theme')
 console.log(theme)
+
+if (theme === 'navy blue') {
+	document.body.style = '#fff'
+}
