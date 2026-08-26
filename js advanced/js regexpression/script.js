cb371ca45f5f3a2ed638716032e443a1dc83a00d
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-26 09:35:05 +0000

    autosave

--- js advanced/js regexpression/script.js
+++ js advanced/js regexpression/script.js
@@ -18,4 +18,4 @@ const password = 'admin1234!.....'
 
 // const date = '2007-04-10'
 console.log(password.replace(//g, '*'))
-console.log((date.replace(/:g/, '-')))
+console.log((date.replace(/:g/, '-')
