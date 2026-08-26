697da79d34aaf46e17060259093c9b9fdd18b878
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-26 01:40:29 +0000

    autosave

--- js advanced/js local storage/script.js
+++ js advanced/js local storage/script.js
@@ -14,6 +14,8 @@ if (theme === 'light') {
 const form = document.querySelector('form')
 form.addEventListener('submit', event => {
 	event.preventDefault()
+
+	const formData = new FormData(form)
 })
 // localStorage.clear() - hammasini o'chiradi
 //localStorage.removeItem('item name')
