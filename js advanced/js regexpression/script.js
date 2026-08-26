afec9f5ddce26404f3d4c5366311aeccf4fb883d
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-26 09:26:51 +0000

    autosave

--- js advanced/js regexpression/script.js
+++ js advanced/js regexpression/script.js
@@ -2,12 +2,13 @@
 
 /* regEx -> /pattern/f
 
+//i - register flag
+//g - global flag
+//m - ko'p qatorli flag
+
 const firstName = 'Sobirjon'
 
 const regEx = /b/ig
 
-//i - register flag
-//g - global flag
-//m - ko'p qatorli flag
 
 console.log(firstName.match(regEx)) //search - birinchi to'g'ri bo'lgan qiymatni aniqlaydi, match - esa hammasini aniqlab chiqaradi */
