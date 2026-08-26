7aa9d86597abe243515577ff077ee1b8bd664415
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-26 09:32:54 +0000

    autosave

--- js advanced/js regexpression/script.js
+++ js advanced/js regexpression/script.js
@@ -13,5 +13,8 @@ const regEx = /b/ig
 
 console.log(firstName.match(regEx)) //search - birinchi to'g'ri bo'lgan qiymatni aniqlaydi, match - esa hammasini aniqlab chiqaradi */
 
+
 const password = 'admin1234!.....'
-console.log(password.replace(/\./g, '*'))
+
+const date = '2007-04-10'
+console.log(password.replace(//g, '*'))
