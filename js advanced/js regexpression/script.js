5e1438c7d39c400e4d5d581316e96d0c9ec98005
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-26 09:24:30 +0000

    autosave

--- js advanced/js regexpression/script.js
+++ js advanced/js regexpression/script.js
@@ -9,4 +9,4 @@ const regEx = /b/i
 //i - register flag
 //g - global flag
 
-console.log(firstName.match(regEx)) //search - birinchi to'g'ri bo'lgan qiymatni aniqlaydi
+console.log(firstName.match(regEx)) //search - birinchi to'g'ri bo'lgan qiymatni aniqlaydi, match - esa hammasini aniqlab chiqaradi
