fa4b48d0124ef3347e80ae556710491c5ae79f53
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-27 04:22:01 +0000

    autosave

--- js advanced/js regexpression/script.js
+++ js advanced/js regexpression/script.js
@@ -15,7 +15,7 @@ console.log(name.search(regEx)) //nechinchi indexda turganini ko'rsatib beradi
 //m - ko'p qatorli flag
 
 const password = 'password123'
-
+console.log(password.replace(/./g))
 
 */
 const email = 'sobirjonkadamboev@gmail.com'
