05eb16420728cc955cd88a9e3d959fa94de514e9
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-27 04:33:24 +0000

    autosave

--- js advanced/js regexpression/script.js
+++ js advanced/js regexpression/script.js
@@ -29,31 +29,4 @@ console.log(date.replace(/-/g, ':')) //bu esa almashtirib beradi
 const email = 'sobirjonkadamboev@gmail.com'
 const regEx = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/'
 
-console.log(regEx.test(email));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const firstName = 'Sobirjon'
-
-const regEx = /b/ig
-
-
-console.log(firstName.match(regEx)) //search - birinchi to'g'ri bo'lgan qiymatni aniqlaydi, match - esa hammasini aniqlab chiqaradi */
-
-
-const password = 'admin1234!.....'
-
-// const date = '2007-04-10'
-console.log(password.replace(//g, '*'))
-console.log((date.replace(/g/, '-')))
+console.log(regEx.test(email))
