441d6a80c01fc234e4a2e0d6c56a2c57b52882b0
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-28 05:08:38 +0000

    autosave

---
+++ js advanced/js webpack/dist/bundle.js
@@ -0,0 +1,33 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	const __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+let __webpack_exports__ = {};
+/*!*******************!*\
+  !*** ./script.js ***!
+  \*******************/
+__webpack_require__.r(__webpack_exports__);
+const modules = require('./another')
+
+const moduleInstance = new modules()
+
+moduleInstance.slider()
+moduleInstance.tabs
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
