a29927446450f4bc35c0d58cd45a58c49fd537c0
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-08-28 05:53:26 +0000

    autosave

--- project/js/script.js
+++ project/js/script.js
@@ -1,45 +1,6 @@
 'use strict'
 
 window.addEventListener('DOMContentLoaded', () => {
-	// Tabs
-
-	const tabs = document.querySelectorAll('.tabheader__item'),
-		tabContents = document.querySelectorAll('.tab_content'),
-		tabParents = document.querySelector('.tabheader__items')
-
-	function hideTabContents() {
-		tabContents.forEach(tabContent => {
-			tabContent.classList.add('hide')
-			tabContent.classList.remove('show')
-		})
-
-		tabs.forEach(tab => {
-			tab.classList.remove('tabheader__item_active')
-		})
-	}
-
-	function showTabContent(index = 0) {
-		tabContents[index].classList.add('show', 'fade')
-		tabContents[index].classList.remove('hide')
-		tabs[index].classList.add('tabheader__item_active')
-	}
-
-	hideTabContents()
-	showTabContent()
-
-	tabParents.addEventListener('click', event => {
-		const target = event.target
-
-		if (target && target.classList.contains('tabheader__item')) {
-			tabs.forEach((tab, index) => {
-				if (target === tab) {
-					hideTabContents()
-					showTabContent(index)
-				}
-			})
-		}
-	})
-
 	// Loader
 
 	const loaderWrapper = document.querySelector('.loader-wrapper')
